acfce4569342e7d5e9ee516f466586e69d4e245c
vitalspace <myglobalsounds@gmail.com>
2023-10-16 17:22:59 -0600

    added dylib binary

--- src/lib.ts
+++ src/lib.ts
@@ -5,7 +5,6 @@ import { encode, toString } from "./encoder";
 const querystring = (querystring: string) => JSON.parse(toString(symbols.parseQueryString(ptr(encode(querystring)))))
 const getKey = (querystring: any, key: string) => toString(symbols.getKey(ptr(encode(querystring)), ptr(encode(key))));
 
-
 export {
     querystring,
     getKey
