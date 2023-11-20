935c4cd91f6cd549385cd1b1d13174440d214fe8
jeremiasbots <98851793+jeremiasbots@users.noreply.github.com>
2023-11-19 23:00:18 -0500

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,19 +1,4 @@
-import { serve, get, post, response, patch } from './src/bunzer'
+import { get, patch, post, serve } from "./src/bunzer.js";
+import { Get, Patch, Post, Param, Request, Req, Header, BodyAttribute, Body, Query, CreateBackend, QueryAttribute, Service } from "./src/Decorators.ts";
 
-get('/hello/some/some', req => 'Hello, World!')
-get('/json', req => ({ hello: 'world' }))
-get('/hello/:name', req => `Hello, ${req.params.name}!`)
-get('/status', req => response(`I'm a teapot`, { status: 418 }))
-get('/setheaders', req => response(`brrrr`, { headers: { 'X-Powered-By': 'bunzer' } }))
-get('/getheaders', req => req.headers['user-agent'])
-get('/ip', req => req.ip)
-get('/query', req => req.query) // /query?limit=10
-get('/error', req => null.ptr)
-get('/async', async req => (await fetch('https://example.com')).text())
-patch("/some", req => ({ hello: 'world' })) // use req.body;
-
-post('/body', req => {
-  const { username, message } = JSON.parse(req.body)
-})
-
-serve({ hostname: '127.0.0.1', port: 8080, public_folder: 'public' })
+export { get, patch, post, serve, Get, Patch, Post, Param, Request, Req, Header, BodyAttribute, Body, Query, CreateBackend, QueryAttribute, Service };
