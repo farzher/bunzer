80bad8311229c16709e856bbecdb61c6f95eddf3
jeremiasbots <98851793+jeremiasbots@users.noreply.github.com>
2023-11-24 12:54:57 -0500

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import { get, patch, post, serve } from "./src/bunzer.js";
+import { get, patch, post, serve, LauriStart } from "./src/bunzer.js";
 import { Get, Patch, Post, Param, Request, Req, Header, BodyAttribute, Body, Query, CreateBackend, QueryAttribute, Service } from "./src/Decorators.ts";
 
-export { get, patch, post, serve, Get, Patch, Post, Param, Request, Req, Header, BodyAttribute, Body, Query, CreateBackend, QueryAttribute, Service };
+export { get, patch, post, serve, Get, Patch, Post, Param, Request, Req, Header, BodyAttribute, Body, Query, CreateBackend, QueryAttribute, Service, LauriStart };
