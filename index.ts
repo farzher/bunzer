d1cc7e4bc8716d131d41b1fc218d490c3d16d40e
jeremiasbots <98851793+jeremiasbots@users.noreply.github.com>
2023-12-06 18:10:21 -0500

    Export any, put, del methods

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import { get, patch, post, serve, LauriStart } from "./src/bunzer.js";
-import { Get, Patch, Post, Param, Req, Header, BodyAttribute, Body, Query, CreateBackend, QueryAttribute, Service } from "./src/Decorators.ts";
+import { get, patch, post, put, del, any, serve, LauriStart } from "./src/bunzer.js";
+import { Get, Patch, Post, Put, Any, Delete, Param, Req, Header, BodyAttribute, Body, Query, CreateBackend, QueryAttribute, Service } from "./src/Decorators.ts";
 
-export { get, patch, post, serve, Get, Patch, Post, Param, Req, Header, BodyAttribute, Body, Query, CreateBackend, QueryAttribute, Service, LauriStart };
+export { get, patch, post, put, del, serve, Get, Patch, Post, Put, Any, Delete, Req, Header, BodyAttribute, Body, Query, CreateBackend, QueryAttribute, Service, LauriStart, Param, any };
